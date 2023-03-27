7d4057e07c794c443c059758e6b8a45eb39f7c69
Dean Andhika Ramadhan <ramadhanrian20@gmail.com>
2023-03-27 13:48:56 +0700

    Upload

---
+++ data/BandaraPulauSulawesi_1.js
@@ -0,0 +1,229 @@
+var json_BandaraPulauSulawesi_1 = {
+  type: "FeatureCollection",
+  name: "BandaraPulauSulawesi_1",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sultan Hasanuddin",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\bandara sultan hasanuddin.jpeg",
+        Lokasi: "Sulawesi Selatan",
+        Landasan: "3.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [119.549786694800375, -5.077654542737098],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sam Ratulangi International",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\bandara sam ratulangi.jpeg",
+        Lokasi: "Sulawesi Utara",
+        Landasan: "2.650 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [124.925530615561343, 1.549933563726544],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Mutiara SIS Al-Jufrie",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\bandara mutiara sis-al jufri.jfif",
+        Lokasi: "Sulawesi Tengah",
+        Landasan: "2.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [119.906449867026637, -0.918580525864018],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Haluoleo",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Haluoleo.jpeg",
+        Lokasi: "Sulawesi Tenggara",
+        Landasan: "2.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [122.417193191795647, -4.076752927153753],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Lagaligo Bua",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Lagaligo Bua.jpg",
+        Lokasi: "Sulawesi Selatan",
+        Landasan: "1.950 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [120.242303683061721, -3.086058444360065],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Djalaluddin",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Djalaluddin.jpg",
+        Lokasi: "Gorontalo",
+        Landasan: "2.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [122.848468223858831, 0.637152435851696],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sangia Nibandera",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\bandara sangia ni bandera.jpeg",
+        Lokasi: "Sulawwesi Tenggara",
+        Landasan: "1.850 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [121.52152944409211, -4.341374013672453],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Tampa Padang",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Tampa Padang.jpeg",
+        Lokasi: "Sulawesi Barat",
+        Landasan: "2.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [119.029049752606568, -2.589055235147758],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sorowako",
+        Gambar: "Bandara/sorowako.jpeg",
+        Lokasi: "Sulawesi Selatan",
+        Landasan: "1.018 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [121.358545320692457, -2.530370972709805],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandaraa Syukuran Aminuddin Amir",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandaraa Syukuran Aminuddin.jpeg",
+        Lokasi: "Sulawesi Tengah",
+        Landasan: "2.250 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [122.77140546289715, -1.040943738892505],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Andi Djemma",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Andi Djemma.jpeg",
+        Lokasi: "Sulawesi Selatan",
+        Landasan: "900 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [120.324149317409166, -2.554020968381109],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Pongtiku",
+        Gambar: "Bandara/pongtiku.jpeg",
+        Lokasi: "Sulawesi Selatan",
+        Landasan: "1.300 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [119.819353679200418, -3.042472313229119],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Pogogul Buol",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\bandara pogogul.jpeg",
+        Lokasi: "Sulawesi Tengah",
+        Landasan: "1.500 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [121.415226226944256, 1.105122731453644],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Kasiguncu",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Kasiguncu.jpeg",
+        Lokasi: "Sulawesi Tengah",
+        Landasan: "1.800 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [120.658262591838039, -1.419133593574347],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sumarorong Mamasa",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Sumarorong Mamasa.jpeg",
+        Lokasi: "Sulawesi Barat",
+        Landasan: "3.772 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [119.303803004566475, -3.148816181099144],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Nama: "Bandara Sultan Bantilan",
+        Gambar:
+          "C:\\Users\\PC\\OneDrive\\Dokumen\\DATA SEMESTER 6\\SIG\\Bandara\\Bandara Sultan Bantilan.jpg",
+        Lokasi: "Sulawesi Tengah",
+        Landasan: "1.400 m",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [120.793516309913343, 1.12172733138469],
+      },
+    },
+  ],
+};
